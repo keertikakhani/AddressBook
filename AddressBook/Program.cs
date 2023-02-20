09cb4f7186f0465444a5071c9d1b1619fd039d0b
Your Name <you@example.com>
2023-02-20 22:25:51 +0530

    Adding a welcome message

---
+++ AddressBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+         public static void Main(string[] args)
+         {
+            Console.WriteLine("Welcome to Address Book Program");
+
+            Console.ReadLine();
+         }
+    }
+}
